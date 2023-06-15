0ca5aec6837365ba0094b1ad04a4b45d0cf566e8
kikirizki <kkrzkrk@gmail.com>
2023-06-15 14:18:35 +0700

    Update dataset downloader script

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p dataset && wget -P dataset/tiny_shakesphere.txt https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt 
+mkdir -p dataset && wget -P dataset/tiny_shakesphere https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt 
