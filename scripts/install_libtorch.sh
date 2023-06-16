71c533dcb3ad1bc0ea31cf2985b61cd18bc403a2
kikirizki <kkrzkrk@gmail.com>
2023-06-16 13:10:04 +0700

    Create install libtorch script

---
+++ scripts/install_libtorch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set the desired LibTorch version and URL
+LIBTORCH_VERSION="2.0.1%2Bcpu"
+LIBTORCH_URL="https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-${LIBTORCH_VERSION}.zip"
+      
+# Set the destination directory
+DESTINATION_DIR="/usr/local/libtorch"
+
+# Download the LibTorch file
+wget $LIBTORCH_URL -O libtorch.zip
+
+# Create the destination directory if it doesn't exist
+mkdir -p $DESTINATION_DIR
+
+# Extract the contents to the destination directory
+unzip libtorch.zip -d $DESTINATION_DIR
+
+# Cleanup - remove the downloaded zip file
+rm libtorch.zip
+
+echo "LibTorch has been downloaded and extracted to $DESTINATION_DIR"
