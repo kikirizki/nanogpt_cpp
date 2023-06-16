1d84ea335d200bd37e5e4a4736d89f392619691c
kikirizki <kkrzkrk@gmail.com>
2023-06-16 13:15:35 +0700

    Update main

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@ int main() {
   size_t block_size = 8;
   auto shakespere_dataset = Dataset(dataset_path, block_size);
 
-  std::string raw_text = "Hello world";
-  auto encoded = shakespere_dataset.encode(raw_text);
+  auto encoded = shakespere_dataset.get_val();
   auto decoded = shakespere_dataset.decode(encoded);
   std::cout<<decoded<<std::endl;
   return 0;
